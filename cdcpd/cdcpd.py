7f900d54ac4573d87298220d67692e08aabd673a
Cheng Chi <chicheng@umich.edu>
2019-04-10 19:02:14 -0400

    added gripper constraint

--- cdcpd/cdcpd.py
+++ cdcpd/cdcpd.py
@@ -23,29 +23,27 @@ class CDCPDParams:
 
 class ConstrainedDeformableCPD:
     def __init__(self, template,
-                 cdcpd_params: CDCPDParams,
-                 optimizer: Optimizer):
+                 cdcpd_params: CDCPDParams):
         """
         Constructor.
         :param template: (M, 3) template for tracking.
         :param cdcpd_params: Type of CDCPDParams
-        :param optimizer: Optimizer used for constrained optimization, subclass of Optimizer.
         """
         self.template = template
         self.cdcpd_params = cdcpd_params
-        self.optimizer = optimizer
         self.M_LLE = None
         if self.cdcpd_params.use_lle:
             self.M_LLE = locally_linear_embedding(
                 self.template, n_neighbors=self.cdcpd_params.lle_neighbors).todense()
 
-    def step(self, point_cloud, mask, cpd_param: CPDParams):
+    def step(self, point_cloud, mask, cpd_param: CPDParams, optimizer: Optimizer):
         """
         Performs one time-step of ConstrainedDeformableCPD.
         :param point_cloud: (H,W,3) point cloud image in camera frame.
         :param mask: (H, W) binary mask for object of interest in point cloud.
         :param cpd_param: CPD parameters used in current time step. Generally, the first time step
         in a tracking sequence should have less regularization (lower beta and lambd) than others.
+        :param optimizer: Optimizer used for constrained optimization, subclass of Optimizer.
         :return: (M, 3) tracking result. Same shape as template.
         """
         filtered_points = point_cloud[mask]
@@ -65,7 +63,7 @@ class ConstrainedDeformableCPD:
         cpd_result = cpd.run()
 
         # Optimization
-        optimization_result = self.optimizer.run(cpd_result)
+        optimization_result = optimizer.run(cpd_result)
 
         # set template for next step
         self.template = optimization_result
--- test/cdcpd_gripper_test.py
+++ test/cdcpd_gripper_test.py
@@ -1,30 +1,46 @@
+import numpy as np
 import pickle
 from cdcpd.cdcpd import CDCPDParams, ConstrainedDeformableCPD
 from cdcpd.cpd import CPDParams
-from cdcpd.optimizer import DistanceConstrainedOptimizer
+from cdcpd.optimizer import PriorConstrainedOptimizer
 from cdcpd.geometry_utils import build_line
 from cdcpd.cv_utils import chroma_key_rope
 from cdcpd.visualization import simple_visualize
 
 
+class InputPack:
+    def __init__(self, input_arr):
+        self.input_arr = input_arr
+
+    def __len__(self):
+        return len(self.input_arr)
+
+    def __getitem__(self, i):
+        return self.input_arr[i][:2]
+
+
 def main():
-    file_path = "data/rope_fast.pk"
-    file_path = "data/rope_simple.pk"
+    # file_path = "/media/chicheng/OSM/data/old_pk/robot_rope_touch_2018-11-20-16-38-13.bag.pk"
+    file_path = "data/gripper_touch.pk"
     input_arr = pickle.load(open(file_path, 'rb'), encoding='bytes')
     template_verts, template_edges = build_line(1.0, 50)
     key_func = chroma_key_rope
 
-    cpd_params = CPDParams(beta=4.0)
+    cpd_params = CPDParams()
     cdcpd_params = CDCPDParams()
-    optimizer = DistanceConstrainedOptimizer(template=template_verts, edges=template_edges)
+    optimizer = PriorConstrainedOptimizer(template=template_verts, edges=template_edges)
 
     cdcpd = ConstrainedDeformableCPD(template=template_verts,
                                      cdcpd_params=cdcpd_params)
 
     tracking_result_history = []
     for i in range(len(input_arr)):
-        point_cloud_img, color_img = input_arr[i]
+        point_cloud_img, color_img, left_gripper, right_gripper = input_arr[i]
         mask_img = key_func(point_cloud_img, color_img)
+
+        prior_pos = np.array([left_gripper[:3, 3], right_gripper[:3, 3]])
+        prior_idx = [0, 49]
+        optimizer.set_prior(prior_pos=prior_pos, prior_idx=prior_idx)
 
         tracking_result = cdcpd.step(point_cloud=point_cloud_img,
                                      mask=mask_img,
@@ -33,11 +49,13 @@ def main():
 
         tracking_result_history.append(tracking_result)
 
-    simple_visualize(input_arr=input_arr,
+    input_pack = InputPack(input_arr)
+    simple_visualize(input_arr=input_pack,
                      tracking_result_history=tracking_result_history,
                      template_edges=template_edges,
                      key_func=key_func,
                      filter_point_cloud=False,
                      target_fps=10)
+
 
 main()
