18a4bac1d83fbf85188a69aedd6b79f1e1094b49
WangYixuan12 <wyx990608@sjtu.edu.cn>
2020-11-14 16:12:47 -0500

    Modified

--- install_scripts/install_cgal.sh
+++ install_scripts/install_cgal.sh
@@ -5,8 +5,8 @@ SRC_REPO_DIR=$INSTALL_DIR/src
 
 mkdir -p $SRC_REPO_DIR; cd $SRC_REPO_DIR
 git clone https://github.com/CGAL/cgal.git
-mkdir -p CGAL/build
-cd CGAL/build
+mkdir -p cgal/build
+cd cgal/build
 git checkout releases/CGAL-5.0.3
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCGAL_HEADER_ONLY=OFF -DCMAKE_BUILD_TYPE=Release -DWITH_CGAL_Qt5=ON ..
 # cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DWITH_OPENMP=ON
