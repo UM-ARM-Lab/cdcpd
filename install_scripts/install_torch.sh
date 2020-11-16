89e47e2c4972da40a8bd53339923654e4d59234a
WangYixuan12 <943023015@qq.com>
2020-11-16 15:31:14 -0800

    modify torch

--- install_scripts/install_torch.sh
+++ install_scripts/install_torch.sh
@@ -6,10 +6,5 @@ SRC_REPO_DIR=$INSTALL_DIR/src
 mkdir -p $SRC_REPO_DIR; cd $SRC_REPO_DIR
 wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
 unzip libtorch-shared-with-deps-latest.zip
-mkdir -p libtorch/build
-cd libtorch/build
-cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-#cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DWITH_OPENMP=ON
-make -j `nproc` -l `nproc`
-make install
+rsync -a libtorch/ ../
 
