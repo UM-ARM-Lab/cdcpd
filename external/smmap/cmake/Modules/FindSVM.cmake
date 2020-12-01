3f9e663913a4d2c65b21eca082e6f2e4691c8421
Peter Mitrano <pmitrano@armstorm>
2020-11-30 20:59:08 -0500

    trying to build on 20.04 without external deps

--- external/smmap/cmake/Modules/FindSVM.cmake
+++ external/smmap/cmake/Modules/FindSVM.cmake
@@ -5,7 +5,6 @@ set(SVM_INCLUDE_DIRS ${SVM_INCLUDE_DIR})
 set(SVM_LIBRARIES ${SVM_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SVM DEFAULT_MSG
-    SVM_LIBRARY SVM_INCLUDE_DIR)
+find_package_handle_standard_args(SVM DEFAULT_MSG SVM_LIBRARY SVM_INCLUDE_DIR)
 
 mark_as_advanced(SVM_LIBRARY SVM_INCLUDE_DIR)
