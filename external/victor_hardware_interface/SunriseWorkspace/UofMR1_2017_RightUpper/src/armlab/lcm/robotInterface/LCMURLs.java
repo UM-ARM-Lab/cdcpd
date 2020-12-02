f2d4815f9861a2addb7b0d10fe7a09ae3906165d
Peter Mitrano <pmitrano@armstorm>
2020-12-02 10:36:03 -0500

    remove unneeded deps

--- external/victor_hardware_interface/SunriseWorkspace/UofMR1_2017_RightUpper/src/armlab/lcm/robotInterface/LCMURLs.java
+++
@@ -1,8 +0,0 @@
-package armlab.lcm.robotInterface;
-
-public class LCMURLs
-{
-	// Right/Upper arm
-	static final public String DEFAULT_DEST_URL = "udp://10.10.10.10:30001";
-	static final public String DEFAULT_SELF_URL = "udp://10.10.10.11:30000";
-}
