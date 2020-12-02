7bb11de9e2c42ab7a2ddfdc5d0a819da3723c991
Peter Mitrano <pmitrano@armstorm>
2020-12-02 11:35:51 -0500

    autoformat with clang-format

--- cdcpd/include/cdcpd/optimizer.h
+++ cdcpd/include/cdcpd/optimizer.h
@@ -27,16 +27,16 @@
 #include <CGAL/AABB_traits.h>
 #include <CGAL/AABB_tree.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel             K;
-typedef K::FT                                                           FT;
-typedef K::Point_3                                                      Point_3;
-typedef K::Ray_3                                                        Ray_3;
-typedef K::Vector_3                                                     Vector;
-typedef CGAL::Surface_mesh<Point_3>                                     Mesh;
-typedef boost::graph_traits<Mesh>::vertex_descriptor                    vertex_descriptor;
-typedef boost::graph_traits<Mesh>::face_descriptor                      face_descriptor;
-typedef CGAL::AABB_face_graph_triangle_primitive<Mesh>                  AABB_face_graph_primitive;
-typedef CGAL::AABB_traits<K, AABB_face_graph_primitive>                 AABB_face_graph_traits;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::FT FT;
+typedef K::Point_3 Point_3;
+typedef K::Ray_3 Ray_3;
+typedef K::Vector_3 Vector;
+typedef CGAL::Surface_mesh<Point_3> Mesh;
+typedef boost::graph_traits<Mesh>::vertex_descriptor vertex_descriptor;
+typedef boost::graph_traits<Mesh>::face_descriptor face_descriptor;
+typedef CGAL::AABB_face_graph_triangle_primitive<Mesh> AABB_face_graph_primitive;
+typedef CGAL::AABB_traits<K, AABB_face_graph_primitive> AABB_face_graph_traits;
 
 
 ////////////////////////////////////////////////////////////////
@@ -49,40 +49,49 @@ typedef CGAL::AABB_traits<K, AABB_face_graph_primitive>                 AABB_fac
 // std::tuple<Eigen::Matrix3Xf, Eigen::Matrix3Xf>
 //    nearest_points_line_segments(const Eigen::Matrix3Xf& last_template, const Eigen::Matrix2Xi& E);
 
-void Wsolver(const Eigen::MatrixXf& P, const Eigen::Matrix3Xf& X, const Eigen::Matrix3Xf& Y, const Eigen::MatrixXf& G, const Eigen::MatrixXf& L, const double sigma2, const double alpha, const double lambda, Eigen::MatrixXf& W);
+void Wsolver(const Eigen::MatrixXf &P, const Eigen::Matrix3Xf &X, const Eigen::Matrix3Xf &Y, const Eigen::MatrixXf &G,
+             const Eigen::MatrixXf &L, const double sigma2, const double alpha, const double lambda,
+             Eigen::MatrixXf &W);
 
 
 class Optimizer
 {
-public:
-    Optimizer(const Eigen::Matrix3Xf _init_temp, const Eigen::Matrix3Xf _last_temp, const float _stretch_lambda, const obsParam& obstacle_param);
-    Optimizer(const Eigen::Matrix3Xf _init_temp, const Eigen::Matrix3Xf _last_temp, const float _stretch_lambda, const std::vector<float> cylinder_data);
-    Optimizer(const Eigen::Matrix3Xf _init_temp, const Eigen::Matrix3Xf _last_temp, const float _stretch_lambda);
+ public:
+  Optimizer(const Eigen::Matrix3Xf _init_temp, const Eigen::Matrix3Xf _last_temp, const float _stretch_lambda,
+            const obsParam &obstacle_param);
 
-    Eigen::Matrix3Xf operator()(const Eigen::Matrix3Xf& Y,
-                                const Eigen::Matrix2Xi& E,
-                                const std::vector<CDCPD::FixedPoint>& fixed_points,
-                                const bool self_intersection = true,
-                                const bool interation_constrain = true);
-private:
-    bool all_constraints_satisfiable(const std::vector<CDCPD::FixedPoint>& fixed_points) const;
-	std::tuple<Eigen::Matrix3Xf, Eigen::Matrix3Xf> nearest_points_and_normal(const Eigen::Matrix3Xf& last_template);
-	std::tuple<Eigen::Matrix3Xf, Eigen::Matrix3Xf> nearest_points_and_normal_cyl(const Eigen::Matrix3Xf& last_template);
-	
-    Eigen::Matrix3Xf initial_template;
-    Eigen::Matrix3Xf last_template;
-    float stretch_lambda;
-	// const Eigen::Matrix3Xf obs_mesh;
-	// const Eigen::Matrix3Xf obs_normal;
-    Mesh::Property_map<face_descriptor, Vector> fnormals;
-    Mesh::Property_map<vertex_descriptor, Vector> vnormals;
-	Mesh mesh;
-    CGAL::AABB_tree<AABB_face_graph_traits> tree;
-	Eigen::Vector3f cylinder_orien;
-	Eigen::Vector3f cylinder_center;
-	float cylinder_radius;
-	float cylinder_height;
-	bool is_shape_comp;
+  Optimizer(const Eigen::Matrix3Xf _init_temp, const Eigen::Matrix3Xf _last_temp, const float _stretch_lambda,
+            const std::vector<float> cylinder_data);
+
+  Optimizer(const Eigen::Matrix3Xf _init_temp, const Eigen::Matrix3Xf _last_temp, const float _stretch_lambda);
+
+  Eigen::Matrix3Xf operator()(const Eigen::Matrix3Xf &Y,
+                              const Eigen::Matrix2Xi &E,
+                              const std::vector<CDCPD::FixedPoint> &fixed_points,
+                              const bool self_intersection = true,
+                              const bool interation_constrain = true);
+
+ private:
+  bool all_constraints_satisfiable(const std::vector<CDCPD::FixedPoint> &fixed_points) const;
+
+  std::tuple<Eigen::Matrix3Xf, Eigen::Matrix3Xf> nearest_points_and_normal(const Eigen::Matrix3Xf &last_template);
+
+  std::tuple<Eigen::Matrix3Xf, Eigen::Matrix3Xf> nearest_points_and_normal_cyl(const Eigen::Matrix3Xf &last_template);
+
+  Eigen::Matrix3Xf initial_template;
+  Eigen::Matrix3Xf last_template;
+  float stretch_lambda;
+  // const Eigen::Matrix3Xf obs_mesh;
+  // const Eigen::Matrix3Xf obs_normal;
+  Mesh::Property_map <face_descriptor, Vector> fnormals;
+  Mesh::Property_map <vertex_descriptor, Vector> vnormals;
+  Mesh mesh;
+  CGAL::AABB_tree<AABB_face_graph_traits> tree;
+  Eigen::Vector3f cylinder_orien;
+  Eigen::Vector3f cylinder_center;
+  float cylinder_radius;
+  float cylinder_height;
+  bool is_shape_comp;
 };
 
 #endif
--- cdcpd/tests/basic_input.cpp
+++ cdcpd/tests/basic_input.cpp
@@ -18,18 +18,18 @@ using Eigen::Matrix2Xi;
 using namespace cv;
 
 
-pcl::visualization::PCLVisualizer::Ptr simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+pcl::visualization::PCLVisualizer::Ptr simpleVis(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
 {
-    // --------------------------------------------
-    // -----Open 3D viewer and add point cloud-----
-    // --------------------------------------------
-    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-    viewer->setBackgroundColor (0, 0, 0);
-    viewer->addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
-    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
-    viewer->addCoordinateSystem (1.0);
-    viewer->initCameraParameters ();
-    return viewer;
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+  viewer->setBackgroundColor(0, 0, 0);
+  viewer->addPointCloud<pcl::PointXYZ>(cloud, "sample cloud");
+  viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+  viewer->addCoordinateSystem(1.0);
+  viewer->initCameraParameters();
+  return viewer;
 }
 
 // pcl::visualization::PCLVisualizer::Ptr rgbVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
@@ -47,101 +47,104 @@ pcl::visualization::PCLVisualizer::Ptr simpleVis (pcl::PointCloud<pcl::PointXYZ>
 //     return viewer;
 // }
 
-void print_mat16(const cv::Mat& m) {
-    for (int v = 0; v < m.rows; ++v)
+void print_mat16(const cv::Mat &m)
+{
+  for (int v = 0; v < m.rows; ++v)
+  {
+    for (int u = 0; u < m.cols; ++u)
     {
-        for (int u = 0; u < m.cols; ++u)
-        {
-            cout << (int)m.at<uint8_t>(v, u) << " ";
-        }
-        cout << endl;
+      cout << (int) m.at<uint8_t>(v, u) << " ";
     }
+    cout << endl;
+  }
 }
 
-int main() {
-    cout << "Test started" << endl;
+int main()
+{
+  cout << "Test started" << endl;
 
-    MatrixXf template_vertices(3, 50);
-    template_vertices.setZero();
-    template_vertices.row(0).setLinSpaced(50, 0, 1);
-    template_vertices.row(2).array() += 1; // TODO this is a hack for avoiding divide by zero issues
+  MatrixXf template_vertices(3, 50);
+  template_vertices.setZero();
+  template_vertices.row(0).setLinSpaced(50, 0, 1);
+  template_vertices.row(2).array() += 1; // TODO this is a hack for avoiding divide by zero issues
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr template_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    for (int i = 0; i < template_vertices.cols(); ++i)
-    {
-        const auto& c = template_vertices.col(i);
-        template_cloud->push_back(pcl::PointXYZ(c(0), c(1), c(2)));
-    }
+  pcl::PointCloud<pcl::PointXYZ>::Ptr template_cloud(new pcl::PointCloud <pcl::PointXYZ>);
+  for (int i = 0; i < template_vertices.cols(); ++i)
+  {
+    const auto &c = template_vertices.col(i);
+    template_cloud->push_back(pcl::PointXYZ(c(0), c(1), c(2)));
+  }
 
-    Matrix2Xi template_edges(2, 49);
-    template_edges(0, 0) = 0;
-    template_edges(1, template_edges.cols() - 1) = 49;
-    for (int i = 1; i <= template_edges.cols() - 1; ++i)
-    {
-        template_edges(0, i) = i;
-        template_edges(1, i - 1) = i;
-    }
+  Matrix2Xi template_edges(2, 49);
+  template_edges(0, 0) = 0;
+  template_edges(1, template_edges.cols() - 1) = 49;
+  for (int i = 1; i <= template_edges.cols() - 1; ++i)
+  {
+    template_edges(0, i) = i;
+    template_edges(1, i - 1) = i;
+  }
 
-    // cout << "template_vertices" << endl;
-    // cout << template_vertices << endl;
-    // cout << "template_edges" << endl;
-    // cout << template_edges << endl;
+  // cout << "template_vertices" << endl;
+  // cout << template_vertices << endl;
+  // cout << "template_edges" << endl;
+  // cout << template_edges << endl;
 
-    FileStorage color_calib_fs("../../../catkin/src/kinect2_calibration_files/data/000792364047/calib_color.yaml", FileStorage::READ);
-    cv::Mat intrinsics;
-    color_calib_fs["cameraMatrix"] >> intrinsics;
-    cout << "intrinsics type: " << intrinsics.type() << endl;
+  FileStorage color_calib_fs("../../../catkin/src/kinect2_calibration_files/data/000792364047/calib_color.yaml",
+                             FileStorage::READ);
+  cv::Mat intrinsics;
+  color_calib_fs["cameraMatrix"] >> intrinsics;
+  cout << "intrinsics type: " << intrinsics.type() << endl;
 
-    CDCPD cdcpd(template_cloud, intrinsics, false);
+  CDCPD cdcpd(template_cloud, intrinsics, false);
 
-    pcl::visualization::PCLVisualizer::Ptr viewer = simpleVis(template_cloud);
-    
-    viewer->spinOnce (100);
+  pcl::visualization::PCLVisualizer::Ptr viewer = simpleVis(template_cloud);
+
+  viewer->spinOnce(100);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  // TODO more
+  for (int i = 0; i < 30; ++i)
+  {
+
+    /// Color filter
+    // For the red rope, (h > 0.85) & (s > 0.5). For the flag, (h < 1.0) & (h > 0.9)
+    // The flag isn't tested or implemented
+    Mat color_image_bgr = imread("../../../catkin/data/image_color_rect_screenshot_10.03.2020.png", IMREAD_COLOR);
+    Mat rgb_image;
+    cv::cvtColor(color_image_bgr, rgb_image, cv::COLOR_BGR2RGB);
+    // TODO I'm pretty sure this is an 8-bit image.
+    Mat depth_image = imread("../../../../data/image_depth_rect_screenshot_10.03.2020.png", IMREAD_UNCHANGED);
+    // cout << "Depth image " << endl;
+    // print_mat16(depth_image);
+
+    //FileStorage depth_calib_fs("../../../kinect2_calibration_files/data/000792364047/calib_color.yaml");
+
+    // cout << rgb_image.cols << " " << rgb_image.rows << endl;
+    // cout << depth_image.cols << " " << depth_image.rows << endl;
+
+    imwrite("rgb.png", rgb_image);
+    imwrite("depth.png", depth_image);
+
+    cv::Mat rgb_f;
+    rgb_image.convertTo(rgb_f, CV_32FC3);
+    rgb_f /= 255.0; // get RGB 0.0-1.0
+    cv::Mat color_hsv;
+    cvtColor(rgb_f, color_hsv, CV_RGB2HSV);
+    cv::Scalar low_hsv = cv::Scalar(.85 * 360.0, 0.5, 0.0);
+    cv::Scalar high_hsv = cv::Scalar(360.0, 1.0, 1.0);
+    cv::Mat hsv_mask;
+    cv::inRange(color_hsv, low_hsv, high_hsv, hsv_mask);
+    cv::imwrite("hsv_mask.png", hsv_mask);
+
+    CDCPD::Output out = cdcpd(rgb_image, depth_image, hsv_mask, template_cloud, template_edges);
+    template_cloud = out.gurobi_output;
+
+    viewer->removePointCloud("sample cloud");
+    viewer->addPointCloud<pcl::PointXYZ>(template_cloud, "sample cloud");
+    viewer->spinOnce(100, true);
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    // TODO more
-    for (int i = 0; i < 30; ++i)
-    {
+  }
 
-        /// Color filter
-        // For the red rope, (h > 0.85) & (s > 0.5). For the flag, (h < 1.0) & (h > 0.9)
-        // The flag isn't tested or implemented
-        Mat color_image_bgr = imread("../../../catkin/data/image_color_rect_screenshot_10.03.2020.png", IMREAD_COLOR);
-        Mat rgb_image;
-        cv::cvtColor(color_image_bgr, rgb_image, cv::COLOR_BGR2RGB);
-        // TODO I'm pretty sure this is an 8-bit image.
-        Mat depth_image = imread("../../../../data/image_depth_rect_screenshot_10.03.2020.png", IMREAD_UNCHANGED);
-        // cout << "Depth image " << endl;
-        // print_mat16(depth_image);
-
-        //FileStorage depth_calib_fs("../../../kinect2_calibration_files/data/000792364047/calib_color.yaml");
-
-        // cout << rgb_image.cols << " " << rgb_image.rows << endl;
-        // cout << depth_image.cols << " " << depth_image.rows << endl;
-
-        imwrite("rgb.png", rgb_image);
-        imwrite("depth.png", depth_image);
-
-        cv::Mat rgb_f;
-        rgb_image.convertTo(rgb_f, CV_32FC3);
-        rgb_f /= 255.0; // get RGB 0.0-1.0
-        cv::Mat color_hsv;
-        cvtColor(rgb_f, color_hsv, CV_RGB2HSV);
-        cv::Scalar low_hsv = cv::Scalar(.85 * 360.0, 0.5, 0.0);
-        cv::Scalar high_hsv = cv::Scalar(360.0, 1.0, 1.0);
-        cv::Mat hsv_mask;
-        cv::inRange(color_hsv, low_hsv, high_hsv, hsv_mask);
-        cv::imwrite("hsv_mask.png", hsv_mask);
-
-        CDCPD::Output out = cdcpd(rgb_image, depth_image, hsv_mask, template_cloud, template_edges);
-        template_cloud = out.gurobi_output;
-
-        viewer->removePointCloud("sample cloud");
-        viewer->addPointCloud<pcl::PointXYZ>(template_cloud, "sample cloud");
-        viewer->spinOnce (100, true);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    }
-
-    cout << "Test ended" << endl;
+  cout << "Test ended" << endl;
 }
