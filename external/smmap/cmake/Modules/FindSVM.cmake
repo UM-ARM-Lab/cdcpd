cc26c85bdeb802df40076d14370b186908f93de5
Peter Mitrano <pmitrano@armstorm>
2020-12-01 22:12:35 -0500

    it compiles

--- external/smmap/cmake/Modules/FindSVM.cmake
+++ external/smmap/cmake/Modules/FindSVM.cmake
@@ -1,10 +1,5 @@
-find_path(SVM_INCLUDE_DIR svm/svm.h)
-set(SVM_LIBRARY ${SVM_INCLUDE_DIR}/../lib/libsvm.so)
+find_path(SVM_INCLUDE_DIR libsvm/svm.h REQUIRED)
+find_library(SVM_LIBRARY libsvm.so REQUIRED)
 
 set(SVM_INCLUDE_DIRS ${SVM_INCLUDE_DIR})
 set(SVM_LIBRARIES ${SVM_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SVM DEFAULT_MSG SVM_LIBRARY SVM_INCLUDE_DIR)
-
-mark_as_advanced(SVM_LIBRARY SVM_INCLUDE_DIR)
