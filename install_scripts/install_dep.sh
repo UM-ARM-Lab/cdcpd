7e0be6ff58efa5a3f1616fc94fe8469eb7e5c8a5
Yixuan Wang <yixuanwa@umich.edu>
2021-01-05 21:56:26 -0800

    install third party library in one script, test needed

---
+++ install_scripts/install_dep.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR=~/.local
+SRC_REPO_DIR=$INSTALL_DIR/src
+USR_NAME=yixuan
+
+echo 'export LD_LIBRARY_PATH=~/.local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PKG_CONFIG_PATH=~/.local/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
+echo 'export PATH=~/.local/bin:${PATH}' >> ~/.bashrc
+echo 'export CMAKE_PREFIX_PATH=~/.local:$CMAKE_PREFIX_PATH' >> ~/.bashrc
+
+sudo apt install libeigen3-dev
+sudo -u $USR_NAME ./install_faiss.sh
+sudo -u $USR_NAME ./install_fgt.sh
+sudo -u $USR_NAME ./install_nomad.sh
+sudo -u $USR_NAME ./install_svm.sh
+sudo -u $USR_NAME ./install_torch.sh
+sudo -u $USR_NAME ./install_ceres.sh
+
