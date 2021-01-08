b53d757990299f42caa8517874dadff29116d0e4
Yixuan Wang <yixuanwa@umich.edu>
2021-01-08 06:55:43 -0800

    debug

--- install_scripts/install_svm.sh
+++ install_scripts/install_svm.sh
@@ -3,6 +3,6 @@ sudo apt install libcgal-qt5-dev -y
 INSTALL_DIR=~/.local
 SRC_REPO_DIR=$INSTALL_DIR/src
 mkdir -p $SRC_REPO_DIR; cd $SRC_REPO_DIR
-git clone https://github.com/dmcconachie/libsvm.git
+git clone https://github.com/WangYixuan12/libsvm.git
 cd libsvm
-make install -j `nproc`
+make install INSTALL_PATH=$INSTALL_DIR -j `nproc`
