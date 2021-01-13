efb8cb948f86526c028b1ccd7d144cead23c6fb6
Peter Mitrano <pmitrano@armstorm>
2021-01-13 09:41:33 -0500

    fixed point are not used  here

--- cdcpd/src/cdcpd.cpp
+++ cdcpd/src/cdcpd.cpp
@@ -1761,14 +1761,13 @@ CDCPD::Output CDCPD::operator()(
     const bool interation_constrain,
     const bool is_prediction,
     const int pred_choice,
-    const std::vector<CDCPD::FixedPoint> &fixed_points)
+    )
 {
   // rgb: CV_8U3C rgb image
   // depth: CV_16U depth image
   // mask: CV_8U mask for segmentation
   // template_cloud: point clouds corresponding to Y^t (Y in IV.A) in the paper
   // template_edges: (2, K) matrix corresponding to E in the paper
-  // fixed_points: fixed points during the tracking
 
   assert(rgb.type() == CV_8UC3);
   if (is_sim)
