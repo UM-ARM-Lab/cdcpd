8b3252a1e321b32107ca22fd2e058063d74c0994
Peter Mitrano <pmitrano@armstorm>
2021-02-03 23:11:24 -0500

    mesh conversion test works

--- cdcpd/src/obs_util.cpp
+++ cdcpd/src/obs_util.cpp
@@ -8,9 +8,9 @@ using Eigen::Matrix3Xf;
 using Eigen::MatrixXf;
 using Eigen::Vector3f;
 
-static Vector3f Pt3toVec(const Point_3 pt)
+Point_3 pointMsgtoPoint(const geometry_msgs::Point pt)
 {
-  return Vector3f(float(pt.x()), float(pt.y()), float(pt.z()));
+  return Point_3(pt.x, pt.y, pt.z);
 }
 
 static Vector3f cgalVec2EigenVec(Vector cgal_v)
@@ -24,26 +24,24 @@ Mesh shapes_mesh_to_cgal_mesh(shape_msgs::Mesh const &input_mesh)
   Mesh output_mesh;
   // first copy all the vertices
   // create a mapping between vertex indices in the input mesh and the output mesh
-  std::unordered_map<unsigned int, CGAL::SM_Vertex_index> vertex_index_map;
-  for (auto vertex_idx = 0u; vertex_idx < input_mesh.vertex_count; ++vertex_idx)
+  std::unordered_map<uint32_t, CGAL::SM_Vertex_index> vertex_index_map;
+  for (auto vertex_idx = 0u; vertex_idx < input_mesh.vertices.size(); ++vertex_idx)
   {
-    auto const vertex = Point_3(input_mesh.vertices[3 * vertex_idx],
-                                input_mesh.vertices[3 * vertex_idx + 1],
-                                input_mesh.vertices[3 * vertex_idx + 2]);
+    auto const vertex = pointMsgtoPoint(input_mesh.vertices[vertex_idx]);
     auto const output_v_index = output_mesh.add_vertex(vertex);
     vertex_index_map.emplace(vertex_idx, output_v_index);
   }
 
   // now copy the faces
-  for (auto face_idx = 0u; face_idx < input_mesh.triangle_count; ++face_idx)
+  for (auto triangle : input_mesh.triangles)
   {
-    auto const input_v_index1 = input_mesh.triangles[3 * face_idx];
-    auto const input_v_index2 = input_mesh.triangles[3 * face_idx + 1];
-    auto const input_v_index3 = input_mesh.triangles[3 * face_idx + 2];
+    auto const input_v_index0 = triangle.vertex_indices[0];
+    auto const input_v_index1 = triangle.vertex_indices[1];
+    auto const input_v_index2 = triangle.vertex_indices[2];
+    CGAL::SM_Vertex_index output_v_index0 = vertex_index_map[input_v_index0];
     CGAL::SM_Vertex_index output_v_index1 = vertex_index_map[input_v_index1];
     CGAL::SM_Vertex_index output_v_index2 = vertex_index_map[input_v_index2];
-    CGAL::SM_Vertex_index output_v_index3 = vertex_index_map[input_v_index3];
-    output_mesh.add_face(output_v_index1, output_v_index2, output_v_index3);
+    output_mesh.add_face(output_v_index0, output_v_index1, output_v_index2);
   }
   return output_mesh;
 }
