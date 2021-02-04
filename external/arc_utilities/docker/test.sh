1d9ac2044d3dbc062e431265150074b3249eb1ed
Peter Mitrano <pmitrano@armstorm>
2021-02-03 22:27:53 -0500

    remove arc_utilities because it's confusing CLion's indexer

--- external/arc_utilities/docker/test.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-ROS_VERSION=${1:-kinetic}
-BUILD_TYPE=${2:-Release}
-
-. /opt/ros/$ROS_VERSION/setup.bash
-. /root/catkin_ws/devel_isolated/setup.bash
-
-cd ~/catkin_ws
-catkin_make_isolated -DCMAKE_BUILD_TYPE=$BUILD_TYPE --catkin-make-args run_tests && catkin_test_results
